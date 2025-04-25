ef3a5b502b39e741e8551d2aaaaf65742bc1d602
AdrianEmaST <desarrolloadrianjr@gmail.com>
2025-04-25 10:41:48 -0300

    Change: Name

--- Frontend/app/layout.tsx
+++ Frontend/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "PsySearch",
+  title: "Insight",
   description: "Todo lo que está bien",
 };
 
