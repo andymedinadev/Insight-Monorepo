d597c6841447078107cdabc66d2562f3648ae47a
AdrianEmaST <desarrolloadrianjr@gmail.com>
2025-04-29 12:15:28 -0300

    Fixed: Layout

--- Frontend/app/dashboard/layout.tsx
+++ Frontend/app/dashboard/layout.tsx
@@ -1,32 +1,33 @@
-"use client";
+'use client'
 
-import { Provider } from "react-redux";
-import { store } from "@/store";
-import { Navbar, Sidebar } from "@/components";
+import { Provider } from 'react-redux'
+import { store } from '@/store'
+import { Navbar, Sidebar } from '@/components'
 
-export default function AuthLayout({
+export default function DashboardLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <Provider store={store}>
-      <div className="">
-        {" "}
-        {/* Usa un div en lugar de html/body */}
-        <div>
-          <Navbar /> {/* Navbar solo se renderiza en el dashboard */}
+      <div className="flex h-screen w-screen overflow-hidden">
+        {/* Sidebar fija al lado izquierdo */}
+        <div className="fixed top-0 left-0 h-full w-64 z-20">
+          <Sidebar />
         </div>
-        <main>
-          <div className="flex flex-row">
-            <div>
-              <Sidebar /> {/* Sidebar solo se renderiza en el dashboard */}
-            </div>
-            <div>{children}</div>
-          </div>
-        </main>{" "}
-        {/* El contenido específico de cada página en el dashboard */}
+
+        {/* Contenido general: navbar + children */}
+        <div className="flex flex-col flex-1 ml-64 w-[calc(100%-16rem)]">
+          {/* Navbar empieza al lado de la sidebar */}
+          <Navbar />
+
+          {/* Contenido scrollable debajo de la navbar */}
+          <main className="flex-1 overflow-y-auto  bg-white">
+            {children}
+          </main>
+        </div>
       </div>
     </Provider>
-  );
+  )
 }
