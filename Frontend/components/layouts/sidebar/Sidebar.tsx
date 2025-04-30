76c5f30d57551fc9e4642c9ef89be9b504f1453d
Nathan <nathan.torresc@gmail.com>
2025-04-30 13:31:53 -0700

    MOD: Mejorar la estructura del formulario de inicio de sesión y agregar enlace a registro

--- Frontend/components/layouts/sidebar/Sidebar.tsx
+++ Frontend/components/layouts/sidebar/Sidebar.tsx
@@ -4,8 +4,7 @@ import Link from 'next/link';
 
 export default function Sidebar() {
   return (
-    <aside className="hidden
-    lg:z-30 lg:flex lg:h-full lg:w-64 lg:flex-col lg:gap-4 lg:border-r lg:bg-white lg:p-6 lg:shadow">
+    <aside className="hidden lg:z-30 lg:flex lg:h-full lg:w-64 lg:flex-col lg:gap-4 lg:border-r lg:bg-white lg:p-6 lg:shadow">
       <h1 className="lg:mb-24 lg:text-xl lg:font-semibold">INSIGHT</h1>
       <Link href="/dashboard/home" className="hover:underline">
         Inicio
