5d80e8067bdea8f8e98b91318ae4c618e6c8a8ed
andymedinadev <107598069+andymedinadev@users.noreply.github.com>
2025-05-01 17:13:51 -0300

    Add: mock data for materials, notes, and patients

---
+++ Frontend/mocks/materials.ts
@@ -0,0 +1,12 @@
+import { Material } from '@/types/material';
+
+export const mockMaterials: Material[] = [
+  { id: 1, date: '10/05/2025', content: 'loremIpsum', title: 'Material N° 1' },
+  { id: 2, date: '11/05/2025', content: 'loremIpsum', title: 'Material N° 2' },
+  { id: 3, date: '12/05/2025', content: 'loremIpsum', title: 'Material N° 3' },
+  { id: 4, date: '13/05/2025', content: 'loremIpsum', title: 'Material N° 4' },
+  { id: 5, date: '14/05/2025', content: 'loremIpsum', title: 'Material N° 5' },
+  { id: 6, date: '15/05/2025', content: 'loremIpsum', title: 'Material N° 6' },
+  { id: 7, date: '16/05/2025', content: 'loremIpsum', title: 'Material N° 7' },
+  { id: 8, date: '17/05/2025', content: 'loremIpsum', title: 'Material N° 8' },
+];
