b0412848d9da188b921c780e30ec77ac7e67d0e4
andymedinadev <107598069+andymedinadev@users.noreply.github.com>
2025-05-01 17:13:40 -0300

    Add: Type definitions for FrequentQuestion, Material, Note, and Patient

--- Frontend/types/index.ts
+++ Frontend/types/index.ts
@@ -3,3 +3,8 @@
 
 export type { default as TypesCardButtons } from './ButtonCards/components';
 export type { default as TypeNewPatient } from './NewPatient/NewPatient';
+
+export type { FrequentQuestion } from './frequentQuestion';
+export type { Material } from './material';
+export type { Note } from './note';
+export type { Patient } from './patient';
---
+++ Frontend/types/material.d.ts
@@ -0,0 +1,6 @@
+export interface Material {
+  content: string;
+  date: string;
+  id: number;
+  title: string;
+}
