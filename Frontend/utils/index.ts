01ba189f1fda0c14650793b24d7a4c4bfe1afff5
andymedinadev <107598069+andymedinadev@users.noreply.github.com>
2025-05-07 13:02:43 -0300

    add: creado componente para poblar backend con Pacientes

--- Frontend/utils/index.ts
+++ Frontend/utils/index.ts
@@ -4,5 +4,6 @@
 // Cards
 export { default as cardData } from './Data/cardData';
 export { mapPatientProfileToUpdatePayload } from './mapPatientProfileToUpdatePayload';
+export { poblarPacientes } from './poblarPacientes';
 export { transformPatientProfileData } from './transformPatientProfileData';
 export { transformFormDataToSignupPayload } from './transformSignupData';
