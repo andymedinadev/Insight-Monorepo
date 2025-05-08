73e3967cd87fb2ca4641194678a74cff7c23afd3
Nathan <nathan.torresc@gmail.com>
2025-05-08 07:34:04 -0700

    Add: wrap logo in a link for navigation in the sidebar

--- Frontend/store/index.ts
+++ Frontend/store/index.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
 import patientReducer from './slices/patientSlice';
+import paginationReducer from './slices/paginationSlice';
 import profileReducer from './slices/profileSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    pagination: paginationReducer,
     patients: patientReducer,
     profile: profileReducer,
   },
