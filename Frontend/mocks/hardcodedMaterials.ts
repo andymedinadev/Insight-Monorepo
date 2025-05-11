0011df8f80871a9af794980dc235d51877819a38
andymedinadev <107598069+andymedinadev@users.noreply.github.com>
2025-05-11 12:53:42 -0300

    ADD: Nuevo tipo paciente todo hardcodeado

---
+++ Frontend/mocks/hardcodedMaterials.ts
@@ -0,0 +1,39 @@
+import { Material } from '@/types/material';
+
+export const mockHardcodedMaterials: Material[] = [
+  {
+    id: 5,
+    date: '20/03/2025',
+    title: 'Material N° 5',
+    content:
+      'Lorem ipsum dolor sit amet, consec tetur adipiscing elit. Etiam finibus blan dit euismod.',
+  },
+  {
+    id: 4,
+    date: '20/03/2025',
+    title: 'Material N° 4',
+    content:
+      'Lorem ipsum dolor sit amet, consec tetur adipiscing elit. Etiam finibus blan dit euismod.',
+  },
+  {
+    id: 3,
+    date: '20/03/2025',
+    title: 'Material N° 3',
+    content:
+      'Lorem ipsum dolor sit amet, consec tetur adipiscing elit. Etiam finibus blan dit euismod.',
+  },
+  {
+    id: 2,
+    date: '20/03/2025',
+    title: 'Material N° 2',
+    content:
+      'Lorem ipsum dolor sit amet, consec tetur adipiscing elit. Etiam finibus blan dit euismod.',
+  },
+  {
+    id: 1,
+    date: '20/03/2025',
+    title: 'Material N° 1',
+    content:
+      'Lorem ipsum dolor sit amet, consec tetur adipiscing elit. Etiam finibus blan dit euismod.',
+  },
+];
--- Frontend/mocks/index.ts
+++ Frontend/mocks/index.ts
@@ -2,3 +2,7 @@ export { mockMaterials } from './materials';
 export { mockNewPatients } from './newPatients';
 export { mockNotes } from './notes';
 export { mockPatients } from './patients';
+
+export { mockHardcodedMaterials } from './hardcodedMaterials';
+export { mockHardcodedNotes } from './hardcodedNotes';
+export { mockHardcodedPatients } from './hardcodedPatients';
