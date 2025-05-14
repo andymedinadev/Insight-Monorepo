49f9d468b9670377dac240659f09a059ca86fa2b
andymedinadev <107598069+andymedinadev@users.noreply.github.com>
2025-05-14 20:57:30 -0300

    add: configurar store para utilizar middleware de persistencia

--- Frontend/store/index.ts
+++ Frontend/store/index.ts
@@ -4,6 +4,39 @@ import patientReducer from './slices/patientSlice';
 import paginationReducer from './slices/paginationSlice';
 import profileReducer from './slices/profileSlice';
 import userReducer from './slices/userSlice';
+import { persistPatientState } from '@/config';
+
+const preloadedPatientsState = {
+  initialized: false,
+  newListDemo: [],
+  raw: [],
+  list: [],
+  searchTerm: '',
+  selected: null,
+  loading: false,
+  error: null,
+  filters: {
+    modalidad: [],
+    genero: [],
+    rangoEtario: [],
+  },
+};
+
+if (typeof window !== 'undefined') {
+  const saved = localStorage.getItem('patientState');
+
+  if (saved) {
+    try {
+      const parsed = JSON.parse(saved);
+
+      preloadedPatientsState.initialized = parsed.initialized || false;
+
+      preloadedPatientsState.newListDemo = parsed.newListDemo || [];
+    } catch (e) {
+      console.error('Error al leer de localStorage:', e);
+    }
+  }
+}
 
 export const store = configureStore({
   reducer: {
@@ -13,6 +46,10 @@ export const store = configureStore({
     profile: profileReducer,
     user: userReducer,
   },
+  preloadedState: {
+    patients: preloadedPatientsState,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(persistPatientState),
 });
 
 // Tipos para usar en componentes con TypeScript
