6ae3770f6c7e173c8b78dd78509aab163198b406
andymedinadev <107598069+andymedinadev@users.noreply.github.com>
2025-05-14 21:22:01 -0300

    fix: minor fixes

--- Frontend/components/EditPatientForm/EditPatientForm.tsx
+++ Frontend/components/EditPatientForm/EditPatientForm.tsx
@@ -453,14 +453,14 @@ export function EditPatientForm() {
           />
         </div>
 
-        <div className="mb-12 inline-flex h-12 items-center justify-center self-stretch rounded-lg bg-[#0655D5]">
-          <button
-            type="submit"
-            className="cursor-pointer justify-start text-center font-['Roboto'] text-base leading-normal font-semibold text-white"
-          >
+        <button
+          type="submit"
+          className="mb-12 inline-flex h-12 cursor-pointer items-center justify-center self-stretch rounded-lg bg-[#0655D5]"
+        >
+          <p className="justify-start text-center font-['Roboto'] text-base leading-normal font-semibold text-white">
             Guardar cambios
-          </button>
-        </div>
+          </p>
+        </button>
       </form>
     </div>
   );
