bb4efdcd001981be9a44e5e81beafff3d037172a
andymedinadev <107598069+andymedinadev@users.noreply.github.com>
2025-05-16 19:54:30 -0300

    fix: borrar hook no utilizado useUpdatePatient

--- Frontend/hooks/index.ts
+++ Frontend/hooks/index.ts
@@ -3,4 +3,3 @@ export { useLogout } from './useLogout';
 export { useNewPatientById } from './useNewPatientById';
 export { usePatientById } from './usePatientById';
 export { useSignup } from './useSignup';
-export { useUpdatePatient } from './useUpdatePatient';
