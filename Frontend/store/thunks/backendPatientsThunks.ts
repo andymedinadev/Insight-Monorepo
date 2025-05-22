cb139cddfec61e2d772204487f29424350e40f9c
andymedinadev <107598069+andymedinadev@users.noreply.github.com>
2025-05-22 16:39:27 -0300

    add: Thunk para creación de pacientes

--- Frontend/store/thunks/backendPatientsThunks.ts
+++ Frontend/store/thunks/backendPatientsThunks.ts
@@ -2,7 +2,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import { BACKEND_BASE_URL } from '@/config';
 import type { RootState } from '../index';
 
-import type { BackendPatient, BackendMaterial, BackendNote } from '@/types';
+import type { BackendPatient, BackendMaterial, BackendNote, BackendNewPatient } from '@/types';
 
 // Traer todos los pacientes
 export const fetchPatients = createAsyncThunk<BackendPatient[], void, { rejectValue: string }>(
@@ -202,6 +202,38 @@ export const fetchOneMaterial = createAsyncThunk<
     const data = await response.json();
     return data;
   } catch (error) {
+    const message = error instanceof Error ? error.message : 'Error desconocido';
+    return thunkApi.rejectWithValue(message);
+  }
+});
+
+// Crear un paciente
+export const createBackendPatient = createAsyncThunk<
+  BackendPatient,
+  BackendNewPatient,
+  { rejectValue: string }
+>('backendPatients/createBackendPatient', async (newPatient, thunkApi) => {
+  const state = thunkApi.getState() as RootState;
+  const token = state.auth.token;
+
+  try {
+    const response = await fetch(`${BACKEND_BASE_URL}/api/Patient`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(newPatient),
+    });
+
+    if (!response.ok) {
+      const errorText = await response.text();
+      throw new Error(`Error al crear paciente: ${errorText}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error: unknown) {
     const message = error instanceof Error ? error.message : 'Error desconocido';
     return thunkApi.rejectWithValue(message);
   }
