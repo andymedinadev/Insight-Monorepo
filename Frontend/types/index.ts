6e3da76cc8cd07ea51232b64cb6cf90179ada5af
andymedinadev <107598069+andymedinadev@users.noreply.github.com>
2025-05-31 21:37:54 -0300

    clean: eliminar tipos no utilizados

--- Frontend/types/index.ts
+++ Frontend/types/index.ts
@@ -3,27 +3,16 @@
 export type { default as TypesCardButtons } from './ButtonCards/components';
 export type { ChangePasswordPayload } from './ChangePassword/ChangePasswordPayload';
 export type { BackendEditPatient } from './backendEditPatient';
-export type { NewPatient } from './newPatient';
 export type { SignupFormData } from './Signup/signupFormData';
 export type { SignupPayload } from './Signup/signupPayload';
 export type { ResetPasswordPayload } from './ResetPassword/ResetPasswordPayload';
 export type { VerifyPayload } from './Signup/verifyPayload';
 export type { VerifyResponse } from './Signup/verifyResponse';
 
-export type { FrequentQuestion } from './frequentQuestion';
-export type {
-  DuracionSesion,
-  HardcodedPatient,
-  ModalityType,
-  RangoEtario,
-  SexType,
-} from './hardcodedPatient';
-export type { Material } from './material';
-export type { Note } from './note';
-export type { Patient } from './patient';
-export type { PatientProfileData } from './patientProfileData';
-export type { UpdatePatientPayload } from './updatePatientPayload';
-
+export type { SexType } from './sexType';
+export type { DuracionSesion } from './duracionSesion';
+export type { ModalityType } from './modalityType';
+export type { RangoEtario } from './rangoEtario';
 export type { DropdownFilterProps } from './Filters/DropdownFilterProps';
 
 export type { BackendMaterial } from './backendMaterial';
